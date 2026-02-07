b83040613ad96f2d66756fee5df0e19357053b22
htripathi <harsh.tripathi@amadeus.com>
2026-02-07 16:21:19 +0530

    Add initial ration distribution system with models and configuration

---
+++ src/main/java/com/ht/management/ration/distribution/RationDistributionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ht.management.ration.distribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RationDistributionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RationDistributionSystemApplication.class, args);
+    }
+
+}
